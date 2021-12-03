7ad78995e88b2afc695e9a7ee8b84463bbfc3632
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-12-02 19:01:27 -0500

    Planets component created

---
+++ src/app/MockPlanets.ts
@@ -0,0 +1,34 @@
+import { Planet } from './Planet';
+
+export const PLANETS: Planet[] = [
+    {
+        id: 1,
+        name: "Earth",
+        size: 12
+    },
+    {
+        id: 2,
+        name: "Saturn",
+        size: 116
+    },
+    {
+        id: 3,
+        name: "Jupiter",
+        size: 139
+    },
+    {
+        id: 4,
+        name: "Venus",
+        size: 12
+    },
+    {
+        id: 5,
+        name: "Neptune",
+        size: 49
+    },
+    {
+        id: 6,
+        name: "Mars",
+        size: 6
+    },
+]
